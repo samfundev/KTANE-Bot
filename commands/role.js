aad770ff26bcefd98a8484e96923e1c7c3b26ebe
samfun123 <samdinner@hotmail.com>
2019-02-02 17:00:16 -0500

    Added whohas command and refactored a bit

--- commands/role.js
+++ commands/role.js
@@ -2,6 +2,32 @@ const commando = require("discord.js-commando");
 const tokens = require("./../tokens");
 const logger = require("./../log");
 
+function getRole(role, assignableData, guildRoles) {
+	let targetRole = role.toLowerCase();
+	for (let roleData of assignableData) {
+		if (roleData.aliases.some(alias => alias.toLowerCase() == targetRole)) {
+			const role = guildRoles.get(roleData.roleID);
+			if (role == undefined) {
+				logger.error(`Unable to find role based on ID: ${roleData.roleID}`);
+				return;
+			}
+
+			return role;
+		}
+	}
+}
+
+function shuffle(a) {
+	var j, x, i;
+	for (i = a.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+	return a;
+}
+
 module.exports = [
 	class RoleCommand extends commando.Command {
 		constructor(client) {
@@ -25,26 +51,16 @@ module.exports = [
 		}
 
 		run(msg, args) {
-			const targetRole = args.role.toLowerCase();
-			for (let roleData of tokens.roleIDs.assignable) {
-				if (roleData.aliases.some(alias => alias.toLowerCase() == targetRole)) {
-					const role = msg.guild.roles.get(roleData.roleID);
-					if (role == undefined) {
-						logger.error(`Unable to find role based on ID: ${roleData.roleID}`);
-						return;
-					}
-
-					if (msg.member.roles.has(roleData.roleID)) {
-						msg.member.removeRole(role)
-							.then(() => msg.channel.send(`Removed the "${role.name}" role from ${msg.author.username}.`))
-							.catch(logger.error);
-					} else {
-						msg.member.addRole(role)
-							.then(() => msg.channel.send(`Gave the "${role.name}" role to ${msg.author.username}.`))
-							.catch(logger.error);
-					}
-
-					break;
+			const role = getRole(args.role, tokens.roleIDs.assignable, msg.guild.roles);
+			if (role !== undefined) {
+				if (msg.member.roles.has(role.id)) {
+					msg.member.removeRole(role)
+						.then(() => msg.channel.send(`Removed the "${role.name}" role from ${msg.author.username}.`))
+						.catch(logger.error);
+				} else {
+					msg.member.addRole(role)
+						.then(() => msg.channel.send(`Gave the "${role.name}" role to ${msg.author.username}.`))
+						.catch(logger.error);
 				}
 			}
 		}
@@ -56,7 +72,7 @@ module.exports = [
 				aliases: ["rolelist", "rl", "roles"],
 				group: "public",
 				memberName: "role-list",
-				description: "DMs you a list of all the roles that can be used with the role command.",
+				description: "Lists of all the roles that can be used with the role command.",
 				examples: ["rolelist", "rl", "roles"],
 				guildOnly: true,
 				throttling: {
@@ -68,6 +84,36 @@ module.exports = [
 
 		run(msg) {
 			msg.channel.send(`Roles:\n${tokens.roleIDs.assignable.map(role => ` - ${role.aliases.join(", ")}`).join("\n")}`);
+		}
+	},
+	class WhoHasRoleCommand extends commando.Command {
+		constructor(client) {
+			super(client, {
+				name: "who-has",
+				aliases: ["whohasrole", "whr", "whohas", "wh", "who", "w"],
+				group: "public",
+				memberName: "who-has",
+				description: "Lists of (at most 10) users who have the specified role.",
+				examples: ["whohas <role>", "wh <role>", "w <role>"],
+				guildOnly: true,
+
+				args: [
+					{
+						key: "role",
+						prompt: "What role do you want to search for?",
+						type: "string"
+					}
+				]
+			});
+		}
+
+		run(msg, args) {
+			const role = getRole(args.role, tokens.roleIDs.assignable, msg.guild.roles);
+			if (role !== undefined) {
+				const members = shuffle(Array.from(role.members.values())).filter((_, index) => index < 10);
+
+				msg.channel.send(`Here is ${members.length} (of ${role.members.size}) users with the ${role.name} role:\n${members.map(member => ` - ${member.user.username}#${member.user.discriminator}`).join("\n")}`);
+			}
 		}
 	}
 ];
