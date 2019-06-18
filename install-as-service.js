29140035a46900234d2785737eccd29222f741ba
Timwi <github@timwi.de>
2019-06-18 15:13:58 +0100

    Commented-out code for uninstalling the service
    (cherry picked from commit 0f74ef8afcc7181cb26b8815ebb8347c1c302c38)

--- install-as-service.js
+++ install-as-service.js
@@ -1,16 +1,25 @@
 var Service = require('node-windows').Service;
- 
+
 // Create a new service object
 var svc = new Service({
   name: 'KTANE Discord bot',
   description: 'The logbot and video announcement bot for the KTANE Discord server.',
   script: 'C:\\Apps\\KTANE Bot\\bot.js'
 });
- 
+
 // Listen for the "install" event, which indicates the
 // process is available as a service.
 svc.on('install',function(){
   svc.start();
 });
- 
+
 svc.install();
+
+// // Listen for the "uninstall" event so we know when it's done.
+// svc.on('uninstall',function(){
+//   console.log('Uninstall complete.');
+//   console.log('The service exists: ', svc.exists);
+// });
+//
+// // Uninstall the service.
+// svc.uninstall();
