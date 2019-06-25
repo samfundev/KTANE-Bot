677aa0581d03b4ba6162ec8fee0c628de47f3395
samfun123 <samdinner@hotmail.com>
2019-06-24 22:03:27 -0400

    Use util.inspect instead of JSON.stringify

--- log.js
+++ log.js
@@ -1,12 +1,10 @@
 const { appendFile } = require("fs");
+const { inspect } = require("util");
 const logFile = "output.log";
 
 const logger = {
 	log: (level = "log", ...data) => {
-		const logMessage = `[${new Date().toISOString()}] [${level}] ${data.map(data => {
-			if (typeof(data) == "object") return JSON.stringify(data);
-			else return data.toString();
-		}).join(", ")}`;
+		const logMessage = `[${new Date().toISOString()}] [${level}] ${data.map(data => typeof data == "string" ? data : inspect(data)).join(" ")}`;
 
 		/* eslint-disable no-console */
 		console[level == "info" ? "log" : level](logMessage);
