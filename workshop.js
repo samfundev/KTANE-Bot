a20a94d8161d6b4969e244a5684bbde13133e277
Timwi <github@timwi.de>
2019-06-25 16:04:15 +0100

    Allow 'manual reprint needed' phrase for major updates

--- workshop.js
+++ workshop.js
@@ -337,7 +337,7 @@ class WorkshopScanner {
 			}
 		};
 
-		const major_regex = /major change|major update|rule[- ]breaking change|manual reprint required|manual reprint necessary|manual update|updated? manual/ig;
+		const major_regex = /major change|major update|rule[- ]breaking change|manual reprint (?:required|necessary|needed)|manual update|updated? manual/ig;
 		const major_matches = matchAll(major_regex, changelog_description);
 		return await this.post_discord(data, major_matches.length > 0);
 	}
