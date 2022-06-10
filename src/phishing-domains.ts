a62884efed4d36f35712754a5d7bfc1125076931
samfundev <samdinner@hotmail.com>
2022-06-10 15:50:57 -0400

    Add error handling and documentation to phishing domains

--- src/phishing-domains.ts
+++ src/phishing-domains.ts
@@ -1,32 +1,47 @@
 import { Message } from "discord.js";
 import got from "./utils/got-traces";
 import * as psl from "psl";
+import Logger from "./log";
 
 let phishingDomains: Set<string> = new Set();
 let lastUpdate: number | null = null;
 
+/**
+ * Check if a message contains a phishing domain.
+ * @param message The message to check for phishing domains.
+ * @returns If the message contains a phishing domain.
+ */
 export default async function checkMessage(message: Message): Promise<boolean> {
 	// Grab the initial list of domains.
 	if (lastUpdate === null) {
-		const response = await got("https://phish.sinking.yachts/v2/all");
-		phishingDomains = new Set(JSON.parse(response.body));
+		try {
+			const response = await got("https://phish.sinking.yachts/v2/all");
+			phishingDomains = new Set(JSON.parse(response.body));
 
-		lastUpdate = Date.now();
+			lastUpdate = Date.now();
+		} catch (err) {
+			Logger.error("Unable to fetch initial phishing domain list.", err);
+			return false;
+		}
 	}
 
 	// Every hour, update the domain list with recently added domains.
 	const difference = (Date.now() - lastUpdate) / 1000;
 	if (difference >= 60 * 60) {
-		// Add a bit of wiggle room, just in case.
-		const response = await got(`https://phish.sinking.yachts/v2/recent/${difference + 60}`);
-		const recentDomains = JSON.parse(response.body) as string[];
+		try {
+			// Add a bit of wiggle room, just in case.
+			const response = await got(`https://phish.sinking.yachts/v2/recent/${difference + 60}`);
+			const recentDomains = JSON.parse(response.body) as string[];
 
-		// Add any new domains.
-		for (const domain of recentDomains) {
-			phishingDomains.add(domain);
+			// Add any new domains.
+			for (const domain of recentDomains) {
+				phishingDomains.add(domain);
+			}
+
+			lastUpdate = Date.now();
+		} catch (err) {
+			Logger.error("Unable to update phishing domain list.", err);
 		}
-
-		lastUpdate = Date.now();
 	}
 
 	// Check the message for URLs and see if their hostname contains a phishing domain.
