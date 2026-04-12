76542183b12de489f3782ba955fbcc9bb8930e14
samfundev <samdinner@hotmail.com>
2026-04-12 12:31:05 -0400

    Track scam channel bursts

--- src/listeners/scams.ts
+++ src/listeners/scams.ts
@@ -3,8 +3,9 @@ import { container, Listener } from "@sapphire/framework";
 import { remove } from "confusables";
 import { Message, EmbedBuilder, Snowflake, ChannelType, escapeMarkdown, resolveColor } from "discord.js";
 import { isModerator, unpartial } from "../bot-utils.js";
-import { DBKey } from "../db.js";
+import { DB, DBKey } from "../db.js";
 import checkMessage from "../phishing-domains.js";
+import TaskManager from "../task-manager.js";
 
 @ApplyOptions<Listener.Options>({ event: "messageCreate" })
 export default class ScamMessageListener extends Listener {
@@ -14,6 +15,8 @@ export default class ScamMessageListener extends Listener {
 		const { client } = this.container;
 		if (!await unpartial(message) || !message.deletable || message.guild === null || message.member === null || message.author.bot || isModerator(message))
 			return;
+
+		this.trackRapidChannelPosting(message);
 
 		const text = [message.content, ...message.embeds.flatMap(embed => [embed.title, embed.description])].join(" ");
 		const content = remove(text).toLowerCase();
@@ -80,4 +83,74 @@ export default class ScamMessageListener extends Listener {
 			}
 		}
 	}
+
+	private readonly recentChannelPosts: { [user: Snowflake]: { [channelID: Snowflake]: number } | undefined } = {};
+	private readonly channelBurstWindowMS = 1000 * 5;
+	private readonly channelBurstThreshold = 3;
+	private readonly maxRecordedBursts = 5000;
+	private readonly timeouts: { [user: Snowflake]: NodeJS.Timeout | undefined } = {};
+
+	private trackRapidChannelPosting(message: Message): void {
+		const userID = message.author.id;
+		const timestamp = Date.now();
+		const earliestTimestamp = timestamp - this.channelBurstWindowMS;
+
+		let posts = this.recentChannelPosts[userID];
+		if (!posts)
+			posts = this.recentChannelPosts[userID] = {};
+
+		posts[message.channel.id] = timestamp;
+
+		// Remove posts that are outside the window unless they've hit the threshold.
+		if (!(userID in this.timeouts)) {
+			for (const [channelId, ts] of Object.entries(posts)) {
+				if (ts < earliestTimestamp)
+					delete posts[channelId];
+			}
+		}
+
+		const uniqueChannelIDs = Object.keys(posts);
+		if (uniqueChannelIDs.length < this.channelBurstThreshold) {
+			return;
+		}
+
+		if (userID in this.timeouts) {
+			clearTimeout(this.timeouts[userID]!);
+		}
+
+		this.timeouts[userID] = setTimeout(() => {
+			this.recordBurst(userID, posts);
+			delete this.recentChannelPosts[userID];
+			delete this.timeouts[userID];
+		}, this.channelBurstWindowMS);
+	}
+
+	private recordBurst(userID: Snowflake, posts: { [channelID: Snowflake]: number }): void {
+		const timeBetweenPosts = Object.values(posts).toSorted().map((timestamp, index, sorted) => index === 0 ? 0 : timestamp - sorted[index - 1]).slice(1);
+
+		const uniqueChannelIDs = Object.keys(posts);
+		const records = container.db.get<ScamChannelBurstRecord[]>(DB.global, "scamChannelBursts", []);
+		records.push({
+			userID,
+			recordedAt: Date.now(),
+			posts,
+			uniqueChannelIDs,
+			timeBetweenPosts
+		});
+
+		if (records.length > this.maxRecordedBursts)
+			records.splice(0, records.length - this.maxRecordedBursts);
+
+		container.db.set(DB.global, "scamChannelBursts", records);
+		const totalTime = timeBetweenPosts.reduce((a, b) => a + b, 0);
+		TaskManager.sendOwnerMessage(`User <@${userID}> posted in ${uniqueChannelIDs.length} channels within a ${this.channelBurstWindowMS / 1000} second window. Total time between posts: ${totalTime} ms.`);
+	}
+}
+
+interface ScamChannelBurstRecord {
+	userID: Snowflake;
+	recordedAt: number;
+	posts: { [channelID: Snowflake]: number };
+	uniqueChannelIDs: Snowflake[];
+	timeBetweenPosts: number[];
 }
